9a8c8b58c4eabec0ecfe05ddaef9ccc0bb612f22
Garrett Wyler <grwyler@gmail.com>
2023-03-25 18:55:43 -0400

    Initial commit

---
+++ climate-sim/src/App.jsx
@@ -0,0 +1,12 @@
+import Map from "./Map";
+// import "leaflet/dist/leaflet.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
