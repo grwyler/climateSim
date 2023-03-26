91c35338b2c53d374b9ce2f7898c84b4c847a3d2
Garrett Wyler <grwyler@gmail.com>
2023-03-26 17:06:27 -0400

    added gh-pages support

--- climate-sim/src/Map.jsx
+++ climate-sim/src/Map.jsx
@@ -6,7 +6,7 @@ function Map() {
     <div className="map">
       <MapContainer
         center={[51.505, -0.09]}
-        zoom={13}
+        zoom={3}
         scrollWheelZoom={true}
         style={{ height: "75vh" }}
       >
@@ -14,7 +14,7 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={[37.0902, -95.7129]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
